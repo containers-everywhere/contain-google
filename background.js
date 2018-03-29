mozilla/contain-facebook

Handle MAC assignment edge-cases

background.js
@@ -6,41 +6,68 @@ const FACEBOOK_DOMAINS = ["facebook.com", "www.facebook.com", "fb.com"];
 
 const MAC_ADDON_ID = "@testpilot-containers";
 
+let macAddonEnabled = false;
 let facebookCookieStoreId = null;
+let facebookCookiesCleared = false;
 
 const facebookHostREs = [];
 
-async function isFacebookAlreadyAssignedInMAC () {
-  let macAddonInfo;
-  // If the MAC add-on isn't installed, return false
+async function isMACAddonEnabled () {
   try {
-    macAddonInfo = await browser.management.get(MAC_ADDON_ID);
+    const macAddonInfo = await browser.management.get(MAC_ADDON_ID);
+    if (macAddonInfo.enabled) {
+      return true;
+    }
   } catch (e) {
     return false;
   }
-  let anyFBDomainsAssigned = false;
-  for (let facebookDomain of FACEBOOK_DOMAINS) {
-    const facebookCookieUrl = `https://${facebookDomain}/`;
-    const assignment = await browser.runtime.sendMessage(MAC_ADDON_ID, {
-      method: "getAssignment",
-      url: facebookCookieUrl
-    });
-    if (assignment) {
-      anyFBDomainsAssigned = true;
-    }
-  }
-  return anyFBDomainsAssigned;
+  return false;
 }
 
-(async function init() {
-  const facebookAlreadyAssigned = await isFacebookAlreadyAssignedInMAC();
-  if (facebookAlreadyAssigned) {
-    return;
-  }
+async function setupMACAddonManagementListeners () {
+  browser.management.onInstalled.addListener(info => {
+    if (info.id === MAC_ADDON_ID) {
+      macAddonEnabled = true;
+    }
+  });
+  browser.management.onUninstalled.addListener(info => {
+    if (info.id === MAC_ADDON_ID) {
+      macAddonEnabled = false;
+    }
+  })
+  browser.management.onEnabled.addListener(info => {
+    if (info.id === MAC_ADDON_ID) {
+      macAddonEnabled = true;
+    }
+  })
+  browser.management.onDisabled.addListener(info => {
+    if (info.id === MAC_ADDON_ID) {
+      macAddonEnabled = false;
+    }
+  })
+}
 
+async function getMACAssignment (url) {
+  try {
+    const assignment = await browser.runtime.sendMessage(MAC_ADDON_ID, {
+      method: "getAssignment",
+      url
+    });
+    return assignment;
+  } catch (e) {
+    return false;
+  }
+}
+
+function generateFacebookHostREs () {
+  for (let facebookDomain of FACEBOOK_DOMAINS) {
+    facebookHostREs.push(new RegExp(`^(.*)?${facebookDomain}$`));
+  }
+}
+
+function clearFacebookCookies () {
   // Clear all facebook cookies
   for (let facebookDomain of FACEBOOK_DOMAINS) {
-    facebookHostREs.push(new RegExp(`^(.*\\.)?${facebookDomain}$`));
     const facebookCookieUrl = `https://${facebookDomain}/`;
 
     browser.cookies.getAll({domain: facebookDomain}).then(cookies => {
@@ -49,54 +76,56 @@ async function isFacebookAlreadyAssignedInMAC () {
       }
     });
   }
+}
 
+async function setupContainer () {
   // Use existing Facebook container, or create one
-  browser.contextualIdentities.query({name: FACEBOOK_CONTAINER_NAME}).then(contexts => {
-    if (contexts.length > 0) {
-      facebookCookieStoreId = contexts[0].cookieStoreId;
-    } else {
-      browser.contextualIdentities.create({
-        name: FACEBOOK_CONTAINER_NAME,
-        color: FACEBOOK_CONTAINER_COLOR,
-        icon: FACEBOOK_CONTAINER_ICON}
-      ).then(context => {
-        facebookCookieStoreId = context.cookieStoreId;
-      });
-    }
-  });
+  const contexts = await browser.contextualIdentities.query({name: FACEBOOK_CONTAINER_NAME})
+  if (contexts.length > 0) {
+    facebookCookieStoreId = contexts[0].cookieStoreId;
+  } else {
+    const context = await browser.contextualIdentities.create({
+      name: FACEBOOK_CONTAINER_NAME,
+      color: FACEBOOK_CONTAINER_COLOR,
+      icon: FACEBOOK_CONTAINER_ICON
+    })
+    facebookCookieStoreId = context.cookieStoreId;
+  }
+}
 
+async function containFacebook (options) {
   // Listen to requests and open Facebook into its Container,
   // open other sites into the default tab context
-  async function containFacebook(options) {
-    const requestUrl = new URL(options.url);
-    let isFacebook = false;
-    for (let facebookHostRE of facebookHostREs) {
-      if (facebookHostRE.test(requestUrl.host)) {
-        isFacebook = true;
-        break;
-      }
+  const requestUrl = new URL(options.url);
+
+  let isFacebook = false;
+  for (let facebookHostRE of facebookHostREs) {
+    if (facebookHostRE.test(requestUrl.host)) {
+      isFacebook = true;
+      break;
     }
-    const tab = await browser.tabs.get(options.tabId);
-    const tabCookieStoreId = tab.cookieStoreId;
-    if (isFacebook) {
-      if (tabCookieStoreId !== facebookCookieStoreId && !tab.incognito) {
-        // See https://github.com/mozilla/contain-facebook/issues/23
-        // Sometimes this add-on is installed but doesn't get a facebookCookieStoreId ?
-        if (facebookCookieStoreId) {
-          browser.tabs.create({
-            url: requestUrl.toString(),
-            cookieStoreId: facebookCookieStoreId,
-            active: tab.active,
-            index: tab.index
-          });
-          browser.tabs.remove(options.tabId);
-          return {cancel: true};
-        }
-      }
-    } else {
-      if (tabCookieStoreId === facebookCookieStoreId) {
+  }
+
+  // We have to check with every request if Facebook is assigned with MAC
+  // because the user can assign it at any given time (needs MAC Events)
+  if (isFacebook && macAddonEnabled) {
+    const facebookAlreadyAssigned = await getMACAssignment(options.url);
+    if (facebookAlreadyAssigned) {
+      // This Facebook URL is assigned with MAC, so we don't handle this request
+      return;
+    }
+  }
+
+  const tab = await browser.tabs.get(options.tabId);
+  const tabCookieStoreId = tab.cookieStoreId;
+  if (isFacebook) {
+    if (tabCookieStoreId !== facebookCookieStoreId && !tab.incognito) {
+      // See https://github.com/mozilla/contain-facebook/issues/23
+      // Sometimes this add-on is installed but doesn't get a facebookCookieStoreId ?
+      if (facebookCookieStoreId) {
         browser.tabs.create({
           url: requestUrl.toString(),
+          cookieStoreId: facebookCookieStoreId,
           active: tab.active,
           index: tab.index
         });
@@ -104,7 +133,26 @@ async function isFacebookAlreadyAssignedInMAC () {
         return {cancel: true};
       }
     }
+  } else {
+    if (tabCookieStoreId === facebookCookieStoreId) {
+      browser.tabs.create({
+        url: requestUrl.toString(),
+        active: tab.active,
+        index: tab.index
+      });
+      browser.tabs.remove(options.tabId);
+      return {cancel: true};
+    }
   }
+}
+
+(async function init() {
+  await setupMACAddonManagementListeners();
+  macAddonEnabled = await isMACAddonEnabled();
+
+  clearFacebookCookies();
+  generateFacebookHostREs();
+  await setupContainer();
 
   // Add the request listener
   browser.webRequest.onBeforeRequest.addListener(containFacebook, {urls: ["<all_urls>"], types: ["main_frame"]}, ["blocking"]);

containers-everywhere/contain-google

background.js
@@ -6,41 +6,68 @@ const GOOGLE_DOMAINS = ["google.com", "google.ch", "google.de", "google.at", "yo
 
 const MAC_ADDON_ID = "@testpilot-containers";
 
+let macAddonEnabled = false;
 let googleCookieStoreId = null;
+let googleCookiesCleared = false;
 
 const googleHostREs = [];
 
-async function isGoogleAlreadyAssignedInMAC () {
-  let macAddonInfo;
-  // If the MAC add-on isn't installed, return false
+async function isMACAddonEnabled () {
   try {
-    macAddonInfo = await browser.management.get(MAC_ADDON_ID);
+    const macAddonInfo = await browser.management.get(MAC_ADDON_ID);
+    if (macAddonInfo.enabled) {
+      return true;
+    }
   } catch (e) {
     return false;
   }
-  let anyGoogleDomainsAssigned = false;
-  for (let googleDomain of GOOGLE_DOMAINS) {
-    const googleCookieUrl = `https://${googleDomain}/`;
-    const assignment = await browser.runtime.sendMessage(MAC_ADDON_ID, {
-      method: "getAssignment",
-      url: googleCookieUrl
-    });
-    if (assignment) {
-      anyGoogleDomainsAssigned = true;
-    }
-  }
-  return anyGoogleDomainsAssigned;
+  return false;
 }
 
-(async function init() {
-  const googleAlreadyAssigned = await isGoogleAlreadyAssignedInMAC();
-  if (googleAlreadyAssigned) {
-    return;
-  }
+async function setupMACAddonManagementListeners () {
+  browser.management.onInstalled.addListener(info => {
+    if (info.id === MAC_ADDON_ID) {
+      macAddonEnabled = true;
+    }
+  });
+  browser.management.onUninstalled.addListener(info => {
+    if (info.id === MAC_ADDON_ID) {
+      macAddonEnabled = false;
+    }
+  })
+  browser.management.onEnabled.addListener(info => {
+    if (info.id === MAC_ADDON_ID) {
+      macAddonEnabled = true;
+    }
+  })
+  browser.management.onDisabled.addListener(info => {
+    if (info.id === MAC_ADDON_ID) {
+      macAddonEnabled = false;
+    }
+  })
+}
 
+async function getMACAssignment (url) {
+  try {
+    const assignment = await browser.runtime.sendMessage(MAC_ADDON_ID, {
+      method: "getAssignment",
+      url
+    });
+    return assignment;
+  } catch (e) {
+    return false;
+  }
+}
+
+function generateGoogleHostREs () {
+  for (let googleDomain of GOOGLE_DOMAINS) {
+    googleHostREs.push(new RegExp(`^(.*)?${googleDomain}$`));
+  }
+}
+
+function clearGoogleCookies () {
   // Clear all google cookies
   for (let googleDomain of GOOGLE_DOMAINS) {
-    googleHostREs.push(new RegExp(`^(.*\\.)?${googleDomain}$`));
     const googleCookieUrl = `https://${googleDomain}/`;
 
     browser.cookies.getAll({domain: googleDomain}).then(cookies => {
@@ -49,54 +76,56 @@ async function isGoogleAlreadyAssignedInMAC () {
       }
     });
   }
+}
 
+async function setupContainer () {
   // Use existing Google container, or create one
-  browser.contextualIdentities.query({name: GOOGLE_CONTAINER_NAME}).then(contexts => {
-    if (contexts.length > 0) {
-      googleCookieStoreId = contexts[0].cookieStoreId;
-    } else {
-      browser.contextualIdentities.create({
-        name: GOOGLE_CONTAINER_NAME,
-        color: GOOGLE_CONTAINER_COLOR,
-        icon: GOOGLE_CONTAINER_ICON}
-      ).then(context => {
-        googleCookieStoreId = context.cookieStoreId;
-      });
-    }
-  });
+  const contexts = await browser.contextualIdentities.query({name: GOOGLE_CONTAINER_NAME})
+  if (contexts.length > 0) {
+    googleCookieStoreId = contexts[0].cookieStoreId;
+  } else {
+    const context = await browser.contextualIdentities.create({
+      name: GOOGLE_CONTAINER_NAME,
+      color: GOOGLE_CONTAINER_COLOR,
+      icon: GOOGLE_CONTAINER_ICON
+    })
+    googleCookieStoreId = context.cookieStoreId;
+  }
+}
 
+async function containGoogle (options) {
   // Listen to requests and open Google into its Container,
   // open other sites into the default tab context
-  async function containGoogle(options) {
-    const requestUrl = new URL(options.url);
-    let isGoogle = false;
-    for (let googleHostRE of googleHostREs) {
-      if (googleHostRE.test(requestUrl.host)) {
-        isGoogle = true;
-        break;
-      }
+  const requestUrl = new URL(options.url);
+
+  let isGoogle = false;
+  for (let googleHostRE of googleHostREs) {
+    if (googleHostRE.test(requestUrl.host)) {
+      isGoogle = true;
+      break;
     }
-    const tab = await browser.tabs.get(options.tabId);
-    const tabCookieStoreId = tab.cookieStoreId;
-    if (isGoogle) {
-      if (tabCookieStoreId !== googleCookieStoreId && !tab.incognito) {
-        // See https://github.com/mozilla/contain-facebook/issues/23
-        // Sometimes this add-on is installed but doesn't get a googleCookieStoreId ?
-        if (googleCookieStoreId) {
-          browser.tabs.create({
-            url: requestUrl.toString(),
-            cookieStoreId: googleCookieStoreId,
-            active: tab.active,
-            index: tab.index
-          });
-          browser.tabs.remove(options.tabId);
-          return {cancel: true};
-        }
-      }
-    } else {
-      if (tabCookieStoreId === googleCookieStoreId) {
+  }
+
+  // We have to check with every request if Google is assigned with MAC
+  // because the user can assign it at any given time (needs MAC Events)
+  if (isGoogle && macAddonEnabled) {
+    const googleAlreadyAssigned = await getMACAssignment(options.url);
+    if (googleAlreadyAssigned) {
+      // This Google URL is assigned with MAC, so we don't handle this request
+      return;
+    }
+  }
+
+  const tab = await browser.tabs.get(options.tabId);
+  const tabCookieStoreId = tab.cookieStoreId;
+  if (isGoogle) {
+    if (tabCookieStoreId !== googleCookieStoreId && !tab.incognito) {
+      // See https://github.com/mozilla/contain-google/issues/23
+      // Sometimes this add-on is installed but doesn't get a googleCookieStoreId ?
+      if (googleCookieStoreId) {
         browser.tabs.create({
           url: requestUrl.toString(),
+          cookieStoreId: googleCookieStoreId,
           active: tab.active,
           index: tab.index
         });
@@ -104,7 +133,26 @@ async function isGoogleAlreadyAssignedInMAC () {
         return {cancel: true};
       }
     }
+  } else {
+    if (tabCookieStoreId === googleCookieStoreId) {
+      browser.tabs.create({
+        url: requestUrl.toString(),
+        active: tab.active,
+        index: tab.index
+      });
+      browser.tabs.remove(options.tabId);
+      return {cancel: true};
+    }
   }
+}
+
+(async function init() {
+  await setupMACAddonManagementListeners();
+  macAddonEnabled = await isMACAddonEnabled();
+
+  clearGoogleCookies();
+  generateGoogleHostREs();
+  await setupContainer();
 
   // Add the request listener
   browser.webRequest.onBeforeRequest.addListener(containGoogle, {urls: ["<all_urls>"], types: ["main_frame"]}, ["blocking"]);